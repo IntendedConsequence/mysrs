271f3382a6ca785d24633097bbe4ad3961dffe70
Irwin1138 <irwin1139@gmail.com>
2011-02-06 16:56:28 +0200

    now exports new cards as well

--- fromanki.py
+++ fromanki.py
@@ -11,11 +11,23 @@ deck.checkDailyStats()
 deck.fillQueues()
 deck.updateNewCountToday()
 
+#reps
 file = codecs.open('repstodo.txt', 'w', encoding="utf-8")
 #file.write(codecs.BOM_UTF8)
 for (id, wtf) in deck.revQueue:
     fact = deck.s.query(anki.facts.Fact).get(deck.cardFromId(id).factId)
     file.write(str(id) + '\t' + fact['Kanji'] + '\t' + fact['Keyword'] + '\t' + fact['Story'] + '\n')
+file.close()
+
+#new cards
+file = codecs.open('repsnew.txt', 'w', encoding="utf-8")
+numNew = 64
+for (id, wtf) in deck.newQueue:
+    if numNew == 0:
+        break
+    fact = deck.s.query(anki.facts.Fact).get(deck.cardFromId(id).factId)
+    file.write(str(id) + '\t' + fact['Kanji'] + '\t' + fact['Keyword'] + '\t' + fact['Story'] + '\n')
+    numNew -= 1
 file.close()
 
 deck.checkDailyStats()
